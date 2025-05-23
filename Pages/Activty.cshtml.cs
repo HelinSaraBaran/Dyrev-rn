0ca83801a00d4b5282519807df095ef17e40b16c
Omar Yasser Soboh <oms001@edu.zealand.dk>
2025-05-23 13:02:21 +0200

    Activity

---
+++ Pages/Activty.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dyreværn.Pages
+{
+    public class ActivtyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
